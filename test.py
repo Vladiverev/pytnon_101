c8a19cba27ed77cc4c9e44686b475aa1a54ce74a
Vladiverev <vladiverev@gmail.com>
2017-12-10 18:39:21 +0200

    delete
    delete

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-data = open('1.json', 'rb')
-response = requests.post(url='https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCCFzMrLK-J2YV9_CqtUBrhsanet0pXyzc',
-    data=data,
-    headers={'Content-Type': 'application/json'})
-print(response.text)
